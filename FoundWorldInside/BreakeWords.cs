4f4ccfdd288417a752b5198c1b554ff1566535ae
yuriishymko <yuriishymko@gmail.com>
2021-11-15 03:34:34 +0200

    Refactoring

---
+++ FoundWorldInside/BreakeWords.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace FoundWordInside
+{
+    internal class BreakeWords
+    {
+        private Dictionary<char, List<string>> _simpleWords = new Dictionary<char, List<string>>();
+
+        /// <summary>
+        /// Specifies the paths to the files
+        /// </summary>
+        /// <param name="pathBreakingWords">Full path to the file to write result of breaking</param>
+        /// <param name="pathWordsForCheck">Full path to the file with word to check</param>
+        /// <param name="pathWordsForDictionary">Full path to the file to populate the dictionary</param>
+        internal void Menu(out string pathBreakingWords, out string pathWordsForCheck, out string pathWordsForDictionary)
+        {
+            while (true)
+            {
+                Console.Clear();
+                pathWordsForCheck = TrimmedLineFromConsole("Enter the full path to the file with word to check: ");
+
+                if (File.Exists(pathWordsForCheck))
+                {
+                    break;
+                }
+                ErrorMessage($"File {pathWordsForCheck} is not Exists ");
+            }
+            pathWordsForDictionary = TrimmedLineFromConsole("Enter the full path to the file to populate the dictionary: ");
+            pathBreakingWords = TrimmedLineFromConsole("Enter the full path to the file to write result of breaking: ");
+        }
+
+        /// <summary>
+        /// Try breaks the term into simple words if possible, otherwise returns without changes
+        /// </summary>
+        /// <param name="FileWithWordToCheck">Full path to the file with word to check</param>
+        /// <param name="FileToWriteBreakedWord">Full path to the file to write result of breaking</param>
+        internal void BreakWords(string FileWithWordToCheck, string FileToWriteBreakedWord)
+        {
+            if (!File.Exists(FileToWriteBreakedWord))
+            {
+                FileToWriteBreakedWord = @" \..\..\..\..\BreakedWord.txt";
+            }
+            if (_simpleWords == null || _simpleWords.Count == 0)
+            {
+                FillingInTheDictionary("");
+            }
+
+            string[] ArrWithNonBreakingWords = File.ReadAllLines(FileWithWordToCheck);
+
+            foreach (var nonBreakingWords in ArrWithNonBreakingWords)
+            {
+                int lengthFoundWords = 0;
+                int countWords = 0;
+                char forCheck = nonBreakingWords[0];
+                string wordForCheck = nonBreakingWords.ToLower();
+                int sizeWordForCheck = nonBreakingWords.Length;
+                List<string> listWithKeyValue = _simpleWords[wordForCheck[0]];
+                StringBuilder sb = new StringBuilder();
+                for (int j = 0; j < sizeWordForCheck && wordForCheck.Length > 0;)
+                {
+                    //if the first letter of the word we are looking for is not the same as the
+                    //letter of the word at the moment, then rewrite List
+                    if (wordForCheck[0] != forCheck)
+                    {
+                        listWithKeyValue = _simpleWords[wordForCheck[0]];
+                        forCheck = wordForCheck[0];
+                    }
+                    else
+                    {
+                        if (!listWithKeyValue.Contains(wordForCheck))
+                        {
+                            wordForCheck = wordForCheck.Substring(0, wordForCheck.Length - 1);
+                        }
+                        else
+                        {
+                            //fix the size of the word found
+                            int wordLength = wordForCheck.Length;
+                            sb.Append(wordForCheck + ',');
+                            countWords++;
+                            j += wordLength;
+                            wordForCheck = nonBreakingWords.Substring(wordForCheck.Length + lengthFoundWords);
+                            //add the size of the found word
+                            lengthFoundWords += wordLength; 
+                        }
+                    }
+                }
+
+                // find everything that did not get in sb
+                string str = nonBreakingWords[(sb.ToString().Length - countWords)..];
+                //check the possibility of the word
+                if (str.Length < 3 && sizeWordForCheck - countWords > 4)
+                {
+                    File.AppendAllText(FileToWriteBreakedWord, $"(in){nonBreakingWords} -> (out){(sb.ToString() + str).TrimEnd(new char[] { ' ', ',' })}" + "\n");
+                }
+                else
+                {
+                    File.AppendAllText(FileToWriteBreakedWord, $"(in){nonBreakingWords} -> (out){nonBreakingWords}" + "\n");
+                }
+            }
+        }
+
+        /// <summary>
+        ///Fills the dictionary with words from the file if it exists otherwise fills with words from the default file
+        /// </summary>
+        /// <param name="parthTofile">Full path to the file to populate the dictionary</param>
+        internal void FillingInTheDictionary(string parthTofile)
+        {
+            if (!File.Exists(parthTofile))
+            {
+                parthTofile = @" \..\..\..\..\GermanWords.txt";
+            }
+            _simpleWords = File.ReadAllLines(parthTofile)
+                                           .Select(m => m.ToLower())
+                                           .GroupBy(o => o[0])
+                                           .ToDictionary(grp => grp.Key, grp => grp.ToList());
+        }
+
+        private void ErrorMessage(string message)
+        {
+            Console.Clear();
+            Console.WriteLine(message);
+            Console.WriteLine("Try again. Press any key to continue...");
+            Console.ReadKey();
+        }
+
+        private string TrimmedLineFromConsole(string message = "")
+        {
+            Console.Write(message);
+            return Console.ReadLine().Trim(new char[] { ' ', '"', '\'' });
+        }
+    }
+}
--- FoundWorldInside/Program.cs
+++ FoundWorldInside/Program.cs
@@ -1,119 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading;
-
-namespace FoundWordInside
+﻿namespace FoundWordInside
 {
-    static class MainClass
+    public class MainClass
     {
-        private static Dictionary<char, List<string>> dictionaryWithSimpleWord = new Dictionary<char, List<string>>();
+        public static void Main(string[] args)
+        {   
+            BreakeWords breakeWords = new BreakeWords();
 
-        private static void Main(string[] args)
-        {
-            Menu(out string roadTofileWithBreakingWords, out string roadTofileWithWordsForCheck, out string roadTofileWithWordsForDictionary);
-            if (File.Exists(roadTofileWithWordsForDictionary))
+            string pathBreakingWords, pathWordsForCheck, pathWordsForDictionary;
+
+            if (args.Length < 2 )
             {
-                FillingInTheDictionary(roadTofileWithWordsForDictionary);
+                breakeWords.Menu(out pathBreakingWords, out pathWordsForCheck, out pathWordsForDictionary);
             }
             else
             {
-                FillingInTheDictionary();
+                pathBreakingWords = GetTrimmed(args[0]);
+                pathWordsForCheck = GetTrimmed(args[1]);
+                pathWordsForDictionary = GetTrimmed(args[2]);
             }
-
-            if (File.Exists(roadTofileWithBreakingWords))
-            {
-                BreakWords(roadTofileWithWordsForCheck, roadTofileWithBreakingWords);
-            }
-            else
-                BreakWords(roadTofileWithWordsForCheck);
+            
+            breakeWords.FillingInTheDictionary(pathWordsForDictionary);
+            breakeWords.BreakWords(pathWordsForCheck,pathBreakingWords);
         }
 
-        private static void Menu(out string roadTofileWithBreakingWords, out string roadTofileWithWordsForCheck, out string roadTofileWithWordsForDictionary)
+        private static string GetTrimmed(string strForTrim)
         {
-            while (true)
-            {
-                Console.Clear();
-
-                Console.Write("Enter the full path to the file to populate the dictionary: ");
-                roadTofileWithWordsForDictionary = Console.ReadLine().Trim(new char[] { ' ', '"', '\'' });
-
-                Console.Write("Enter the full path to the file to write result of breaking: ");
-                roadTofileWithBreakingWords = Console.ReadLine().Trim(new char[] { ' ', '"', '\'' });
-
-                Console.Write("Enter the full path to the file with word to check: ");
-                roadTofileWithWordsForCheck = Console.ReadLine().Trim(new char[] { ' ', '"', '\'' });
-                if (File.Exists(roadTofileWithWordsForCheck))
-                {
-                    break;
-                }
-                else
-                {
-                    Console.Clear();
-                    Console.WriteLine($"File {roadTofileWithWordsForCheck} is not Exists ");
-                    Console.WriteLine("Try again");
-                    Thread.Sleep(1500);
-                }
-            }
-        }
-
-        private static void BreakWords(string FileWithWordToCheck, string FileToWriteBreakedWord = @" \..\..\..\BreakedWord.txt")
-        {
-            string[] ArrWithNonBreakingWords = File.ReadAllLines(FileWithWordToCheck);
-
-            for (int i = 0; i < ArrWithNonBreakingWords.Length; i++)
-            {
-                var (a, b, c) = (0, 0, 0);
-                char forcheck = ArrWithNonBreakingWords[i][0];
-                string wordForCheck = ArrWithNonBreakingWords[i].ToLower();
-                int sizeWordForCheck = ArrWithNonBreakingWords[i].Length;
-                List<string> listWithKeyValue = dictionaryWithSimpleWord[wordForCheck[0]];
-                StringBuilder sb = new StringBuilder();
-                for (int j = 0; j < sizeWordForCheck && wordForCheck.Length > 0;)
-                {
-                    if (wordForCheck[0] != forcheck)
-                    {
-                        listWithKeyValue = dictionaryWithSimpleWord[wordForCheck[0]];
-                        forcheck = wordForCheck[0];
-                    }
-                    else
-                    {
-                        if (!listWithKeyValue.Contains(wordForCheck))
-                        {
-                            wordForCheck = wordForCheck.Substring(0, wordForCheck.Length - 1);
-                        }
-                        else
-                        {
-                            b = wordForCheck.Length;
-                            sb.Append(wordForCheck + ',');
-                            c++;
-                            j += wordForCheck.Length;
-                            wordForCheck = ArrWithNonBreakingWords[i].Substring(wordForCheck.Length + a);
-                            a += b;
-                        }
-                    }
-                }
-
-                string str = ArrWithNonBreakingWords[i][(sb.ToString().Length - c)..];
-                if (str.Length < 3 && sizeWordForCheck - c > 4)
-                {
-                    File.AppendAllText(FileToWriteBreakedWord, $"(in){ArrWithNonBreakingWords[i]} -> (out){(sb.ToString() + str).TrimEnd(new char[] { ' ', ',' })}" + "\n");
-                }
-                else
-                {
-                    File.AppendAllText(FileToWriteBreakedWord, $"(in){ArrWithNonBreakingWords[i]} -> (out){ArrWithNonBreakingWords[i]}" + "\n");
-                }
-            }
-        }
-
-        private static void FillingInTheDictionary(string parthTofile = @" \..\..\..\..\GermanWords.txt")
-        {
-            dictionaryWithSimpleWord = File.ReadAllLines(parthTofile)
-            .Select(m => m.ToLower())
-            .GroupBy(o => o[0])
-            .ToDictionary(grp => grp.Key, grp => grp.ToList());
+            return strForTrim.Trim(new char[] { ' ', '"', '\'' });
         }
     }
 }
